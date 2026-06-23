ff6f1fff78c29029bfe7e2a6e9aa404b0af6940a
AnshulGammaEdge <anshul.sahu@gammaedge.io>
2026-06-23 15:18:00 +0530

    Updated

--- tests/test_github_action.py
+++ tests/test_github_action.py
@@ -4,5 +4,6 @@ from playwright.sync_api import expect
 def test_github(page):
     page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
     page.get_by_role("link",name="Blog").click()
+    page.locator("input.gsc-input").fill("What Is Artificial Intelligence?")
     var=page.locator("h3.post-title entry-title")
     expect(var).to_be_visible()
